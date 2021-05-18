9f90adca0cb60e5d7f3314c4ffdff8cd3899948a
RedCr0w <1421699947@qq.com>
2021-05-19 00:51:19 +0800

    L>>F UPD
    2021年5月19日 00:51:16

---
+++ src/main/java/com/dongs/dongsfinal/enums/PosCapEnum.java
@@ -0,0 +1,25 @@
+package com.dongs.dongsfinal.enums;
+
+/**
+ * Description:用来返回货架容量
+ * Date: 2021/4/27
+ */
+public enum PosCapEnum {
+
+    CAPACITY(20);
+
+    Integer Shelfcapacity;
+
+    public Integer getShelfcapacity() {
+        return Shelfcapacity;
+    }
+
+    public void setShelfcapacity(Integer shelfcapacity) {
+        Shelfcapacity = shelfcapacity;
+    }
+
+    PosCapEnum(Integer Shelfcapacity){
+        this.Shelfcapacity = Shelfcapacity;
+    }
+
+}

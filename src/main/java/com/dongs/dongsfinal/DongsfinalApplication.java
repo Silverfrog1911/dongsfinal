fcb32539e453eb1546e76dfa443ec9e2b9366652
RedCr0w <1421699947@qq.com>
2021-05-19 00:35:44 +0800

    Initial commit

---
+++ src/main/java/com/dongs/dongsfinal/DongsfinalApplication.java
@@ -0,0 +1,13 @@
+package com.dongs.dongsfinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DongsfinalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DongsfinalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dongs/dongsfinal/DongsfinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dongs.dongsfinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DongsfinalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
